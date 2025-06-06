3ca4437ebbd837d92df77d45272848887054f722
Shimabukuro114 <ritoki-shimabukuro@gr.paramix.co.jp>
2025-06-06 13:56:59 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+
+	public String familiyName = "" ;
+	public String givenName ="";
+	public String address = "";
+	
+	public void commonIntroduce() {
+		
+	}
+	
+	abstract public void eachIntroduce();
+	
+	public void execIntroduce() {
+		
+	}
+}
